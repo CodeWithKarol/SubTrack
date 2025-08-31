95fa93ef49f4d59ec7ccb4045bd7d5996220967e
CodeWithKarol <kmodelski93@gmail.com>
2025-08-31 13:23:49 +0200

    feat: Add main application routes
    Defines routes for subscriptions, expenses, and reminders components. Adds default and wildcard redirects to the subscriptions route.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'subscriptions', loadComponent: () => import('./subscription/subscription').then((c) => c.Subscription)},
+  { path: 'expenses', loadComponent: () => import('./expense/expense').then((c) => c.Expense)},
+  { path: 'reminders', loadComponent: () => import('./reminder/reminder').then((c) => c.Reminder)},
+  { path: '', redirectTo: 'subscriptions', pathMatch: 'full' },
+  { path: '**', redirectTo: 'subscriptions'}
+];
