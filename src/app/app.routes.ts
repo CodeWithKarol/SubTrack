2928b2a82da1813eb5bbca48b5f1d61256409f20
CodeWithKarol <kmodelski93@gmail.com>
2025-08-31 13:28:33 +0200

    chore: Add Prettier and format app.routes.ts
    Prettier was added as a dev dependency to improve code formatting consistency. The app.routes.ts file was reformatted for better readability and alignment with Prettier standards.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: 'subscriptions', loadComponent: () => import('./subscription/subscription').then((c) => c.Subscription)},
-  { path: 'expenses', loadComponent: () => import('./expense/expense').then((c) => c.Expense)},
-  { path: 'reminders', loadComponent: () => import('./reminder/reminder').then((c) => c.Reminder)},
+  {
+    path: 'subscriptions',
+    loadComponent: () => import('./subscription/subscription').then((c) => c.Subscription),
+  },
+  { path: 'expenses', loadComponent: () => import('./expense/expense').then((c) => c.Expense) },
+  { path: 'reminders', loadComponent: () => import('./reminder/reminder').then((c) => c.Reminder) },
   { path: '', redirectTo: 'subscriptions', pathMatch: 'full' },
-  { path: '**', redirectTo: 'subscriptions'}
+  { path: '**', redirectTo: 'subscriptions' },
 ];
